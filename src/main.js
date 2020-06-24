b4f65f2d0ee13673c45e8c22b9dd8c347d844391
Radoslav Radev <radoslav@auxcode.com>
2020-06-24 16:57:10 +0300

    only the first container to be without margin-top

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { BootstrapVue } from 'bootstrap-vue'
+import VueCarousel from 'vue-carousel';
 
 Vue.use(BootstrapVue)
+Vue.use(VueCarousel)
 Vue.config.productionTip = false
 
 new Vue({
