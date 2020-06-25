05a8dcf0a2275d1ad644098fe00b08b0f6d88482
Milen Markov <m1@auxcode.com>
2020-06-25 16:22:50 +0300

    Added new paragraph "place" and the new design is done.

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -2,17 +2,15 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-          <h2>Team</h2>
+        <h2>{{title}}</h2>
       </div>
     </div>
-    <!-- V-for for team members TODO (Milen) -->
     <div class="row content">
       <div class="col-4" v-for="(person) of people" :key="person.name">
         <img :src="person['avatar-link']" class="person-avatar" />
-        <h3>{{person.name}}</h3>
-        <p class="person-description">
-          {{person.description}}
-        </p>
+        <h3 class="person-name">{{person.name}}</h3>
+        <p class="person-place">{{person.place}}</p>
+        <p class="person-description">{{person.description}}</p>
       </div>
     </div>
   </div>
@@ -24,20 +22,47 @@ export default {
   name: "CustomerReviews",
   data() {
     return {
-      title: '',
-      people: []
-    }
+      title: "",
+      people: [],
+      place: ""
+    };
   },
   mounted() {
     this.title = team.title;
-    this.people = team.people
+    this.people = team.people;
+    this.place = team.place;
   }
 };
 </script>
 
 <style scoped lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap");
+@import "../../public/assets/main.scss";
 .person-description {
+  font-family: $descriptions__fonts;
+  font-size: 18px;
+  color: #474a67;
   width: 75%;
   margin: 0 auto;
+}
+
+.container {
+  margin-top: 450px;
+}
+.col-12 {
+  font-family: $family__fonts;
+  font-size: 40px;
+  color: #1e2f67;
+}
+.person-name {
+  font-family: $family__fonts;
+  font-size: 25px;
+  color: #1e2f67;
+  margin-top: 60px;
+}
+.person-place {
+  font-family: "Rubik", sans-serif;
+  font-size: 16px;
+  color: #9597ac;
 }
 </style>
