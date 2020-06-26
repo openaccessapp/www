114783f0ce32499598701fc4d761c5386ce5eed1
Milen Markov <m1@auxcode.com>
2020-06-26 14:35:11 +0300

    Added footer components

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,47 @@
 <template>
   <div class="container-fluid">
-    <div class="row"></div>
+    <div class="row container-links">
+      <div class="col-3">
+        <router-link to="/TermsAndConditions">Page Name</router-link>
+      </div>
+      <div class="col-3">
+        <a :href="page1">Page Name</a>
+      </div>
+      <div class="col-3">
+        <a :href="page1">Page Name</a>
+      </div>
+      <div class="col-3">
+        <a :href="page1">Page Name</a>
+      </div>
+    </div>
+    <div class="row container-links">
+      <div class="col-3">
+        <a :href="page5">Page Name</a>
+      </div>
+    </div>
     <img src="assets/logo-text.png" />
   </div>
 </template>
 
 <script>
+import footer from "js-yaml-loader!../../content-manager/footer.yaml";
 export default {
   name: "CustomerReviews",
   data() {
-    return {};
+    return {
+      page1: "",
+      page2: "",
+      page3: "",
+      page4: "",
+      page5: ""
+    };
+  },
+  mounted() {
+    this.page1 = footer["page-1"];
+    this.page2 = footer["page-2"];
+    this.page3 = footer["page-3"];
+    this.page4 = footer["page-4"];
+    this.page5 = footer["page-5"];
   }
 };
 </script>
