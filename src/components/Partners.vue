4d0449efb804be9f670d4f366390cf93fcf36931
Mihael Nikolov <mn@auxcode.com>
2020-09-01 20:02:00 +0300

    Fixed Partners component

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -1,61 +1,65 @@
 <template>
   <div class="container partners">
-    <div class="row">
+    <div class="row title">
       <p class="header">{{text}}</p>
     </div>
-    <div class="row">
+    <div class="row information">
       <div
         class="col-xl-3 col-lg-4 col-md-6 col-sm-12 col-12 partners-cards"
         v-for="partners in partners"
         :key="partners"
       >
-        <img class="logo" :src="partners.image" />
-        <p class="title">{{partners.title}}</p>
-        <p class="description">{{partners.description}}</p>
-        <div class="lines" />
-        <div class="contacts">
-          <div class="center">
-            <p class="name">{{partners.name}}</p>
-            <div class="row">
-              <img :src="partners['email-image']" />
-              <p>{{partners.email}}</p>
-            </div>
-            <div class="row">
-              <img :src="partners['number-image']" />
-              <p>{{partners.number}}</p>
-            </div>
-            <div class="row">
-              <img :src="partners['twitter-image']" />
-              <p>{{partners.twitter}}</p>
+        <div class="partners-content">
+          <img class="logo" :src="partners.image" />
+          <p class="title">{{partners.title}}</p>
+          <p class="description">{{partners.description}}</p>
+          <div class="lines" />
+          <div class="contacts">
+            <div class="center">
+              <p class="name">{{partners.name}}</p>
+              <div class="row">
+                <img :src="partners['email-image']" />
+                <p>{{partners.email}}</p>
+              </div>
+              <div class="row">
+                <img :src="partners['number-image']" />
+                <p>{{partners.number}}</p>
+              </div>
+              <div class="row">
+                <img :src="partners['twitter-image']" />
+                <p>{{partners.twitter}}</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="row">
+    <div class="row information">
       <div
         class="col-xl-3 col-lg-4 col-md-6 col-sm-12 col-12 partners-cards"
         v-for="partners in partners"
         :key="partners"
       >
-        <img class="logo" :src="partners.image" />
-        <p class="title">{{partners.title}}</p>
-        <p class="description">{{partners.description}}</p>
-        <div class="lines" />
-        <div class="contacts">
-          <div class="center">
-            <p class="name">{{partners.name}}</p>
-            <div class="row">
-              <img :src="partners['email-image']" />
-              <p>{{partners.email}}</p>
-            </div>
-            <div class="row">
-              <img :src="partners['number-image']" />
-              <p>{{partners.number}}</p>
-            </div>
-            <div class="row">
-              <img :src="partners['twitter-image']" />
-              <p>{{partners.twitter}}</p>
+        <div class="partners-content">
+          <img class="logo" :src="partners.image" />
+          <p class="title">{{partners.title}}</p>
+          <p class="description">{{partners.description}}</p>
+          <div class="lines" />
+          <div class="contacts">
+            <div class="center">
+              <p class="name">{{partners.name}}</p>
+              <div class="row">
+                <img :src="partners['email-image']" />
+                <p>{{partners.email}}</p>
+              </div>
+              <div class="row">
+                <img :src="partners['number-image']" />
+                <p>{{partners.number}}</p>
+              </div>
+              <div class="row">
+                <img :src="partners['twitter-image']" />
+                <p>{{partners.twitter}}</p>
+              </div>
             </div>
           </div>
         </div>
@@ -97,20 +101,30 @@ export default {
 <style lang="scss">
 @import "../../public/assets/main.scss";
 .partners {
-  .row {
+  &.container {
+    margin-top: 0 !important;
+  }
+  .header {
+    font-family: $font__family;
+    font-size: 40px;
+    color: #1e2f67;
+    text-align: center;
+    margin: 280px auto 85px;
+    align-items: center;
+  }
+
+  .row.information {
     align-items: center;
     // align-items: baseline;
     justify-content: center;
-    display: flex;
-    .header {
-      font-family: $font__family;
-      font-size: 40px;
-      color: #1e2f67;
-      text-align: center;
-      margin-bottom: 180px;
-    }
+    height: 445px;
+
     .partners-cards {
-      margin-bottom: 300px;
+      height: 100%;
+      justify-content: center;
+      display: flex;
+      flex-direction: column;
+
       .logo {
         width: 135px;
       }
@@ -135,10 +149,13 @@ export default {
       }
     }
     .partners-cards:hover {
-      padding: 20px 10px 20px;
+      padding: 0 10px 20px;
       box-shadow: 6px 10px 35px #1e2f671a;
       border-radius: 30px;
       margin-bottom: 0px;
+      div.partners-content {
+        height: 100%;
+      }
       .description,
       .title {
         display: flex;
@@ -153,6 +170,7 @@ export default {
       .logo {
         width: 105px;
         height: 145px;
+        padding-top: 0;
       }
       .contacts {
         display: flex;
@@ -179,6 +197,9 @@ export default {
           }
         }
       }
+    }
+    &.title {
+      height: auto;
     }
   }
 }
