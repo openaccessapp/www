761cedf4a6afbce4732aa13b9335ae25dd2fa1c7
Radoslav Radev <radoslav@auxcode.com>
2020-12-09 20:41:46 +0100

    show the first file

--- src/components/documentation/Tree.vue
+++ src/components/documentation/Tree.vue
@@ -6,6 +6,7 @@
 
 <script>
 import NodeTree from "./NodeTree";
+import { EventBus } from '../../event-bus';
 
 export default {
   props: {
@@ -13,6 +14,28 @@ export default {
   },
   components: {
     NodeTree
+  },
+  watch: {
+    treeData(newProp){
+      console.log(newProp)
+      this.openContent(newProp)
+    }
+  },
+  methods: {
+    openContent(node) {
+      let path = (this.findFile(node)).path;
+      path = ((path?.split('/'))?.slice(3, path?.length)).join('/');
+      EventBus.$emit('open-content', path)
+    },
+    findFile(node) {
+      if(node && node.type === 'file'){
+        return node;
+      } else if(node.type === 'directory' && node.children?.length > 0) {
+        for(let child of node.children) {
+          return this.findFile(child);
+        }
+      }
+    }
   }
 };
 </script>
